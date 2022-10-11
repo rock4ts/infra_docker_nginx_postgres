f2019fc35c5ab0bbc9ecd5f08a39243f8719e2d7
Artyom Sukhov <rocket.sukhov@gmail.com>
2022-10-11 20:27:19 +0300

    sukhov/feature/create_base_models

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, GenreTitle, Title
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    search_fields = ('name', 'slug',)
+
+
+class AdminGenre(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    search_fields = ('name', 'slug',)
+
+
+class AdminGenreInline(admin.TabularInline):
+    model = GenreTitle
+
+
+class AdminTitle(admin.ModelAdmin):
+    fields = ('name', 'category', 'year',)
+    readonly_fields = ('year',)
+    inlines = (AdminGenreInline,)
+    list_display = ('name', 'year', 'category', 'get_genres')
+    search_fields = ('name', 'year', 'category__name', 'genre__name')
+    list_filter = ('category', 'genre',)
+
+    def get_genres(self, obj):
+        return [genre.genre_id for genre in obj.genres.all()]
+    get_genres.short_description = 'Жанр'
+
+
+admin.site.register(Category, AdminCategory)
+admin.site.register(Genre, AdminGenre)
+admin.site.register(Title, AdminTitle)
---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,61 @@
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField('Название произведения', max_length=100)
+    year = models.IntegerField(
+        'Год публикации',
+        default=datetime.date.today().year
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        verbose_name='Категория',
+        related_name='titles', blank=True, null=True
+    )
+    genre = models.ManyToManyField(
+        Genre, through='GenreTitle', verbose_name='Жанр'
+    )
+
+
+class GenreTitle(models.Model):
+    title_id = models.ForeignKey(
+        Title, on_delete=models.CASCADE,
+        related_name='genres', verbose_name=''
+    )
+    genre_id = models.ForeignKey(
+        Genre, on_delete=models.CASCADE,
+        related_name='titles', verbose_name=''
+    )
+
+    class Meta:
+        verbose_name_plural = 'Жанры'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title_id', 'genre_id'],
+                name='unique_constraint_fail',
+            ),
+        ]
