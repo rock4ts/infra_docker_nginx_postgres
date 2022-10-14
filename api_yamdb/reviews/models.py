d649b99af132ab3dd6fb34562ce6ac6c44ebf1b8
Максим Рябов <maximorya@gmail.com>
2022-10-14 04:03:13 +0400

    модели отзыва и комментария

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,97 +3,72 @@ from django.db import models
 
 User = get_user_model()
 class Review(models.Model):
-    text = models.TextField(
-        verbose_name='Текст отзыва',
-        help_text='Сами письмена отзыва'
-    )
-
-    created = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата комментария',
-        help_text='день-месяц-год'
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='review',
-        verbose_name='Автор отзыва',
-        help_text='Привязка к автору отзыва'
-    )
     title = models.ForeignKey(
-        Title,
+        'Title',
         on_delete=models.CASCADE,
-        related_name='title',
-        verbose_name='Произведение',
-        help_text='К какому произведению отзыв'
+        related_name='reviews',
+        verbose_name='Произведение'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ],
+        verbose_name='Оценка'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
     )
 
     class Meta:
-        ordering = ('-created',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique review')
+        ]
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
 
     def __str__(self):
-        return self.text[:15]
+        return self.text
 
 
 class Comment(models.Model):
-    text = models.TextField(
-        verbose_name='Текст комментария',
-        help_text='Сами письмена комента'
-    )
-    created = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Дата комментария',
-        help_text='день-месяц-год'
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='comments',
-        verbose_name='Автор комментария',
-        help_text='Привязка к автору комментария'
-    )
     review = models.ForeignKey(
-        Review,
-        on_delete=models.CASCADE,
+        'Review', on_delete=models.CASCADE,
         related_name='comments',
-        verbose_name='Комментарий к отзыву',
-        help_text='К какому отзыву коммент'
+        verbose_name='Отзыв'
+    )
+    text = models.TextField(verbose_name='Текст')
+    author = models.ForeignKey(
+        'User', on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
     )
 
     class Meta:
-        ordering = ('-created',)
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
 
     def __str__(self):
-        return self.text[:15]
+        return self.text
 
-class Point(models.Model):
-    #Нужно продумать запрет на многоразовое добавление оценки
-    title = models.ForeignKey(
-        Title,
-        on_delete=models.CASCADE,
-        related_name='raiting_point',
-        verbose_name='оценка произведения',
-        help_text='выставленный поизведению балл'
-    )
 
-    weight = models.IntegerField(
-        verbose_name='Оценка произведения',
-        help_text='Число от 1 до 10 для оценивания'
-    )
+ROLE_CHOICES = (
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+)
 
-    #Продумываю запрет ставить повторную оценку произведению
-    class Meta:
-        verbose_name = 'Оценка'
-        verbose_name_plural = 'Оценки'
-        constraints = [
-            models.UniqueConstraint(
-                fields=('user', 'weight'),
-                name='unique point'),
-            models.CheckConstraint(
-                check=~models.Q(user=models.F('user')),
-                name='do not repeatpoint'),
-        ]
